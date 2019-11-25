3adabbb8abc46e228ecbc384c049c427497b6069
czy211 <czy211@hotmail.com>
2019-11-25 22:45:27 +0800

    重命名变量和生成的文件名

--- src/main/java/com/github/czy211/wowapi/main/Main.java
+++ src/main/java/com/github/czy211/wowapi/main/Main.java
@@ -5,17 +5,15 @@ import com.github.czy211.wowapi.util.Utils;
 
 class Main {
     public static void main(String[] args) {
-        Page global = new Page("function", "/World_of_Warcraft_API", "GlobalFunctions.lua", "AttemptToSaveBindings");
-        global.crawl();
+        Page WoWAPI = new Page("function", "/World_of_Warcraft_API", "WoWAPI.lua", "AttemptToSaveBindings");
+        WoWAPI.crawl();
 
-        Page widget = new Page("function", "/Widget_API", "WidgetFunctions.lua");
-        widget.crawl();
-        if (widget.getOutputPath() != null) {
-            String src = widget.getOutputPath() + "/wow-api/" + widget.getFileName();
-            Utils.addHeader(src, Utils.generateWidgetTypesAndFunc(src));
-        }
+        Page widgetAPI = new Page("function", "/Widget_API", "WidgetAPI.lua");
+        widgetAPI.crawl();
+        String src = widgetAPI.getOutputPath() + "/wow-api/" + widgetAPI.getFileName();
+        Utils.addHeader(src, Utils.generateWidgetTypesAndFunc(src));
 
-        Page lua = new Page("function", "/Lua_functions", "LuaFunctions.lua");
-        lua.crawl();
+        Page luaAPI = new Page("function", "/Lua_functions", "LuaAPI.lua");
+        luaAPI.crawl();
     }
 }
