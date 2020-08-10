1b61db4be5c06b2e49b663829dd14d4e36e40df3
czy211 <czy211@hotmail.com>
2020-08-10 19:33:17 +0800

    修改配置文件位置和默认下载位置

--- src/main/java/com/github/czy211/wowapi/constant/PathConst.java
+++ src/main/java/com/github/czy211/wowapi/constant/PathConst.java
@@ -1,7 +1,5 @@
 package com.github.czy211.wowapi.constant;
 
-import java.io.File;
-
 public class PathConst {
     public static final String ROOT_PATH;
     public static final String CONFIG;
@@ -10,12 +8,9 @@ public class PathConst {
 
     static {
         String path = PathConst.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        File file = new File(path);
-        if (path.endsWith(".jar") && file.isFile()) {
-            ROOT_PATH = path.substring(0, path.lastIndexOf("/") + 1);
-        } else {
-            ROOT_PATH = path;
-        }
+        int lastSlashIndex = path.lastIndexOf("/");
+        int secondLastSlashIndex = path.lastIndexOf("/", lastSlashIndex - 1);
+        ROOT_PATH = path.substring(0, secondLastSlashIndex + 1);
         CONFIG = ROOT_PATH + "conf/";
         CONFIG_FILE = CONFIG + "config.properties";
         DEFAULT_DOWNLOAD = ROOT_PATH + "downloads/";
