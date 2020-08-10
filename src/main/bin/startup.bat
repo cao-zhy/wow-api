a6c931cc27663a4836f0c392f4a17ced989bc6e7
czy211 <czy211@hotmail.com>
2020-08-10 18:28:46 +0800

    打包项目

--- src/main/bin/startup.bat
+++ src/main/bin/startup.bat
@@ -5,7 +5,7 @@ if "%JAVA_HOME%" == "" (
     pause
 ) else (
     cd %~dp0
-    "%JAVA_HOME%\bin\java" -jar ..\lib\wow-api-2.0.5.jar
+    "%JAVA_HOME%\bin\java" -jar ..\lib\wow-api-3.0.0.jar
 
     if errorlevel 1 (
         pause
