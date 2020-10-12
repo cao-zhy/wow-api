056be7e7ea43d8f91fadd842f67bc899417b843d
czy211 <czy211@hotmail.com>
2020-10-12 14:33:33 +0800

    build: 更新版本 3.2.0

--- src/main/bin/startup.bat
+++ src/main/bin/startup.bat
@@ -5,7 +5,7 @@ if "%JAVA_HOME%" == "" (
     pause
 ) else (
     cd %~dp0
-    "%JAVA_HOME%\bin\java" -Dfile.encoding=utf-8 -jar ..\lib\wow-api-3.1.3.jar
+    "%JAVA_HOME%\bin\java" -Dfile.encoding=utf-8 -jar ..\lib\wow-api-3.2.0.jar
 
     if errorlevel 1 (
         pause
