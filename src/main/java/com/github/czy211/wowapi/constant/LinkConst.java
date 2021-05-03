8bdfde65f79dac24cfea96c2c4a421f958370d4e
czy211 <czy211@hotmail.com>
2021-05-03 15:36:10 +0800

    chore: 新建分支 v3.3.0

--- src/main/java/com/github/czy211/wowapi/constant/LinkConst.java
+++
@@ -1,6 +0,0 @@
-package com.github.czy211.wowapi.constant;
-
-public class LinkConst {
-    public static final String WIKI_BASE = "https://wow.gamepedia.com";
-    public static final String FXML_BASE = "https://www.townlong-yak.com/framexml";
-}
--- src/main/java/com/github/czy211/wowapi/constant/PropConst.java
+++
@@ -1,6 +0,0 @@
-package com.github.czy211.wowapi.constant;
-
-public class PropConst {
-    public static final String DOWNLOAD_PATH = "download_path";
-    public static final String BIC_PATH = "bic_path";
-}
